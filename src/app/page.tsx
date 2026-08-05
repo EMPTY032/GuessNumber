8e7a8a2e05648c6636f904d019283e08d180b9f2
Empty0 <empty0@Noutbuk-Empty0.local>
2026-08-05 21:45:25 +0300

    first_commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,55 @@
-import Image from "next/image";
+"use client";
+
+import { SyntheticEvent, useRef, useState } from "react";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function Page() {
+  const inputRef = useRef<null | HTMLInputElement>(null);
+
+  const [activeGame, setActiveGame] = useState<boolean | null>(false);
+  const [savedNumber, setSavedNumber] = useState<number | null>();
+  const [text, setText] = useState<string>("Жду ввода");
+
+  let guessNumber = Math.floor(Math.random() * 100) + 1;
+
+  const handleSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    setSavedNumber(Number(inputRef.current?.value));
+
+    if (savedNumber! < guessNumber) {
+      setText("Ваше число меньше загаданного");
+    }
+    if (savedNumber! > guessNumber) {
+      setText("Ваше число больше загаданного");
+    }
+    if (savedNumber! === guessNumber) {
+      setText("Ура, вы угадали!");
+    }
+  };
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>
-            To get started, edit the{" "}
-            <code className={styles.code}>page.tsx</code> file.
-          </h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <section className={styles.section}>
+      <h1>Вам необходимо угадать число от 1 до 100</h1>
+      <h2>Нажмите ниже чтобы начать</h2>
+
+      <p>загаданное число {guessNumber}</p>
+      <p>сохраненное число {savedNumber}</p>
+
+      {activeGame ? (
+        <div>
+          <p title="типо ждун)))))))">{text}</p>
+
+          <form onSubmit={handleSubmit}>
+            <input type="number" min={1} max={100} ref={inputRef} />
+
+            <button type="submit">sdcsdc</button>
+          </form>
+          <button onClick={() => setActiveGame(false)}>назад</button>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      ) : (
+        <button onClick={() => setActiveGame(true)}>Нажми на меня!</button>
+      )}
+    </section>
   );
 }
